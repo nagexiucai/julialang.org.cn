9be5d6597a4d7d470c661824268228db092aa655
nagexiucai <nagexiucai@qq.com>
2018-09-29 12:25:53 +0800

    Julia作为专用标识出现时首字母要大写；强调人名时按大驼峰。

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-sm-12 col-xs-12">
-                <p class="text-md-left text-sm-left text-xs-left"><a href="https://julialang.org/" target="_blank">julia</a></p>
+                <p class="text-md-left text-sm-left text-xs-left"><a href="https://julialang.org/" target="_blank">Julia</a></p>
             </div>
             <div class="col-md-4 col-sm-12 col-xs-12">
                 <?php
@@ -11,7 +11,7 @@
 
                 <p class="text-md-center text-sm-left text-xs-left {{ $extra_class  }}">
                     @if ($_SERVER['REQUEST_URI'] === '/')
-                        Special thanks to <a href="http://github.com/summerblue/" target="_blank">summerblue</a>
+                        Special thanks to <a href="http://github.com/summerblue/" target="_blank">SummerBlue</a>
                     @endif
                 </p>
             </div>
